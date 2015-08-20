a3675935510379a55b746606583998241fa5a157
Jeff <jahuangi@yahoo.ca>
2015-08-19 17:24:46 -0700

    working jukebox check-in. todo: resolve timing bug

---
+++ jukebox.js
@@ -0,0 +1,32 @@
+
+function parseNote(singleNoteStr) {
+  noteObj = new Object();
+  noteArr = singleNoteStr.split("*");
+  noteObj.pitch = noteArr[0];
+  noteObj.beats = 1;
+  if (noteArr[1] !== undefined) {
+      noteObj.beats = parseInt(noteArr[1]);
+  };
+  return noteObj;
+};
+
+function parseSong(songStr) {
+  songArr = songStr.split(" ");
+  console.log(songArr);
+  songArr = songArr.map(parseNote);
+  return songArr;
+};
+var onComplete = function () {
+  alert("Song finished playing");
+}
+
+var FREQUENCY = 400;
+songStr = "A*2 Bb*3 B*3 C*3 C#*3 D E F G F E D C#*3 C*3 B*3 Bb*3 A*2";
+while (songStr !== "") {
+  songStr = prompt("Enter a song string (Empty String to quit): ");
+  if (songStr !== "") { 
+    songArr = parseSong(songStr);
+    playSong(songArr,FREQUENCY,onComplete);
+  };
+}
+
